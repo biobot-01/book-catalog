6c9f1d2698a7cb4c1264e58a022bb0a85b534514
Kevin Michael <biobot.mike@gmail.com>
2019-09-05 13:16:02 +0200

    Add BookInstance Model

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 from django.urls import reverse
 
@@ -44,3 +45,43 @@ class Book(models.Model):
     def get_absolute_url(self):
         """Returns the url to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
+
+
+class BookInstance(models.Model):
+    """Model representing a specific copy of a book.
+    It can be borrowed from the library
+    """
+
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        help_text=('Unique ID for this particular book across '
+                   'the whole library'),
+    )
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        blank=True,
+        default='m',
+        help_text='Book availability',
+    )
+
+    class Meta:
+        """Model to order objects when they are returned in a query."""
+
+        ordering = ['due_back']
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.id} ({self.book.title})'
