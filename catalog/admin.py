accf41c2885f23dd0cacd9de4e7f4d8bb1dfec5d
Kevin Michael <biobot.mike@gmail.com>
2019-09-15 20:42:18 +0200

    Add ModelAdmin class for Author, Book, BookInstance

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
 from .models import Author, Book, BookInstance, Genre, Language
 
-admin.site.register(Author)
-admin.site.register(Book)
-admin.site.register(BookInstance)
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    """Admin class for the Author model."""
+
+    pass
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    """Admin class for the Book model."""
+
+    pass
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    """Admin class for the BookInstance model."""
+
+    pass
+
+
 admin.site.register(Genre)
 admin.site.register(Language)
